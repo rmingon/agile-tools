b7d829985b69b72a018e62d48df61f86e2a59a7a
rmingon <ronanmingon69550@gmail.com>
2023-04-09 23:10:46 +0200

    add prisma models

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ConfigService} from "@nestjs/config";
+import {PrismaService} from "./prisma.service";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app)
+
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
   await app.listen(port);
