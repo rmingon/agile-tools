af5261c3f6f23f784118e4959cbd12a83bb5f118
rmingon <ronanmingon69550@gmail.com>
2023-04-09 22:54:33 +0200

    add cors

--- front/app.vue
+++ front/app.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <h1 class="text-3xl font-bold underline">
-      Hello world!
+      {{ test }}
     </h1>
   </div>
 </template>
+
+<script setup lang="ts">
+  import {onMounted} from "@vue/runtime-core";
+
+  const test = ref({})
+  onMounted(async () => {
+    const response = await fetch('http://localhost:3001')
+    console.log("Download complete", response);
+    test.value = response
+  })
+</script>
