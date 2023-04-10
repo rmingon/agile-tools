bc4a5145839be728c29ea399745d6c7dad4e79d2
rmingon <ronanmingon69550@gmail.com>
2023-04-10 17:10:47 +0200

    add auth module + service

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from "@nestjs/config";
+import { UsersModule } from './users/users.module';
 import configuration from "../config/configuration";
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     load: [configuration],
-  })],
+  }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
