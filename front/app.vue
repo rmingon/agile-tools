42280ecf9426ae4708419a8c41a0040c87c6c7a5
rmingon <ronanmingon69550@gmail.com>
2023-04-11 16:38:43 +0200

    add landing page

--- front/app.vue
+++ front/app.vue
@@ -1,18 +1,10 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      {{ test }}
-    </h1>
-  </div>
+    <NuxtLayout v-if="route_name !== ''" >
+        <NuxtPage />
+    </NuxtLayout>
+    <NuxtPage v-else/>
 </template>
 
 <script setup lang="ts">
-  import {onMounted} from "@vue/runtime-core";
-
-  const test = ref({})
-  onMounted(async () => {
-    const response = await fetch('http://localhost:3001')
-    console.log("Download complete", response);
-    test.value = response
-  })
+    const route_name = useRoute().name
 </script>
