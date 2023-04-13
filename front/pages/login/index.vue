5c62f83aa021bc12bd823b8842d472a5bef54d7c
rmingon <ronanmingon69550@gmail.com>
2023-04-13 15:58:38 +0200

    add login page

---
+++ front/pages/login/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="w-full h-screen flex">
+    <div class="bg-red-200 w-1/2">
+
+    </div>
+    <div class="w-1/2">
+      <div class="mx-auto w-1/3 my-40">
+        <KeepAlive>
+          <component :is="QUERY_TO_ROUTE[route.query.type]"/>
+        </KeepAlive>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+
+  import SignUp from "~/components/login/SignUp.vue";
+  import Signin from "~/components/login/SignIn.vue";
+
+  import { useRoute } from 'vue-router'
+
+  const route = useRoute()
+
+  const QUERY_TO_ROUTE = {
+      'SignIn': Signin,
+      'SignUp': SignUp
+  }
+
+</script>
