061fc3b3e7ad34f18c5a57997beef84c624d9bf1
rmingon <ronanmingon69550@gmail.com>
2023-04-15 16:36:01 +0200

    fix sign in

--- front/components/login/SignIn.vue
+++ front/components/login/SignIn.vue
@@ -1,5 +1,8 @@
 <template>
-    <h1 class="font-bold">Login</h1>
+    <div class="flex items-center space-x-2">
+      <h1 class="font-bold">Login</h1>
+      <img src="/img/icon/icon-smile.png" alt="icon smile" class="w-10">
+    </div>
     <p class="text-gray-400">Welcome back, please login to your account</p>
     <form action="" method="post" class="space-y-3 mt-5">
         <div>
@@ -15,20 +18,28 @@
                     <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M2.54 4.71L3.25 4L20 20.75l-.71.71l-3.34-3.35c-1.37.57-2.87.89-4.45.89c-4.56 0-8.5-2.65-10.36-6.5c.97-2 2.49-3.67 4.36-4.82L2.54 4.71M11.5 18c1.29 0 2.53-.23 3.67-.66l-1.12-1.13c-.73.5-1.6.79-2.55.79C9 17 7 15 7 12.5c0-.95.29-1.82.79-2.55L6.24 8.41a10.64 10.64 0 0 0-3.98 4.09C4.04 15.78 7.5 18 11.5 18m9.24-5.5C18.96 9.22 15.5 7 11.5 7c-1.15 0-2.27.19-3.31.53l-.78-.78C8.68 6.26 10.06 6 11.5 6c4.56 0 8.5 2.65 10.36 6.5a11.47 11.47 0 0 1-4.07 4.63l-.72-.73c1.53-.96 2.8-2.3 3.67-3.9M11.5 8C14 8 16 10 16 12.5c0 .82-.22 1.58-.6 2.24l-.74-.74c.22-.46.34-.96.34-1.5A3.5 3.5 0 0 0 11.5 9c-.54 0-1.04.12-1.5.34l-.74-.74c.66-.38 1.42-.6 2.24-.6M8 12.5a3.5 3.5 0 0 0 3.5 3.5c.67 0 1.29-.19 1.82-.5L8.5 10.68c-.31.53-.5 1.15-.5 1.82Z"/></svg>
                 </div>
             </div>
+          <div class="flex font-light justify-between items-center text-gray-500 my-3">
+            <div class="items-center flex">
+              <input type="checkbox" name="" id="" class="mr-1">
+              Remember me
+            </div>
+            <div>
+              <NuxtLink to="/login?type=recovery-password" >Forgot Password?</NuxtLink>
+            </div>
+          </div>
         </div>
         <div>
-            <input type="submit" value="Sign Up" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-light rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none w-full cursor-pointer">
+            <input @click="submit" type="submit" value="Sign Up" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-light rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none w-full cursor-pointer">
         </div>
     </form>
     <div>
         Don't have account ?
-        <NuxtLink to="/login?type=SignUp" class="text-blue-800 text-bold">Create an account</NuxtLink>
+        <NuxtLink to="/login?type=sign-up" class="text-blue-800 text-bold">Create an account</NuxtLink>
     </div>
 </template>
 <script setup lang="ts">
 
 const password_showed = ref(false)
 const password_type = computed(() => password_showed.value ? 'text' : 'password')
-
 
 </script>
