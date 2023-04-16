6eda0f4d79f26e55ba2805d3a4a5a1a753979506
rmingon <ronanmingon69550@gmail.com>
2023-04-16 16:02:46 +0200

    add sidebar face

--- front/components/Sidebar.vue
+++ front/components/Sidebar.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="h-screen">
-      Sidebar
+  <div>
+    <div class="h-16 bg-amber-100">
+      // LOGO
+    </div>
+    <div class="flex-col">
+      <div v-for="tab in tabs" class="h-10 flex items-center bg-blue-100 px-2 cursor-pointer text-sm border-r-4 border-blue-600 hover:bg-blue-100 transform duration-100 hover:border-r-2 hover:border-blue-600">
+        <div class="mr-2 text-blue-600">
+          <svg width="24" height="24" viewBox="0 0 32 32"><path d="M28 4H4a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h24a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm0 22H4V6h24z" fill="currentColor"/><path d="M21 20h-8a3.51 3.51 0 0 0-.88-1.86l3.8-6.64l-1.74-1l-3.78 6.62a3.35 3.35 0 0 0-.9-.12a3.5 3.5 0 1 0 3.15 5H21zM9.5 22a1.5 1.5 0 1 1 1.5-1.5A1.5 1.5 0 0 1 9.5 22z" fill="currentColor"/><path d="M19 8h7v2h-7z" fill="currentColor"/><path d="M19 12h4v2h-4z" fill="currentColor"/></svg>
+        </div>
+        <p class="text-blue-600">{{tab.label}}</p>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+  interface Tabs {
+    label: string
+    active: boolean
+  }
+  const tabs : Tabs[] = [
+    {
+      label: "Projects",
+      active: false
+    },
+    {
+      label: "Users",
+      active: false
+    }
+  ]
+</script>
