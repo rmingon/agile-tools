329a0d8e8d8343af291be0920d177e3c11091fad
rmingon <ronanmingon69550@gmail.com>
2023-04-16 16:03:24 +0200

    add topbar style

--- front/components/Topbar.vue
+++ front/components/Topbar.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="h-screen">
+  <div class="flex items-center h-16 w-full">
+    <div class="rounded-xl mx-4 my-3 h-12 bg-amber-100 w-full">
       Topbar
+    </div>
   </div>
 </template>
