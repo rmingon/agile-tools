2e0aa83abf75aef11fa06be063d8638463012bbb
rmingon <ronanmingon69550@gmail.com>
2023-04-16 16:02:08 +0200

    add user interface

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="flex">
-        <Sidebar/>
-        <slot />
+      <Sidebar class="h-screen w-1/5"/>
+      <div class="flex-col w-full bg-gray-100">
+        <Topbar/>
+        <div class="h-screen">
+          <slot />
+        </div>
+      </div>
     </div>
 </template>
+
+<script setup>
+
+</script>
