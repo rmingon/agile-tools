3e3a99af1519aa6df63eee60b5f1f348542fea62
rmingon <ronanmingon69550@gmail.com>
2023-04-17 08:04:10 +0200

    up default layout

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="flex">
-      <Sidebar class="h-screen w-1/5"/>
-      <div class="flex-col w-full bg-gray-100">
+      <Sidebar class="h-screen w-1/6"/>
+      <div class="flex-col w-full bg-gray-200">
         <Topbar/>
-        <div class="h-screen">
+        <div class="min-h-screen p-4">
           <slot />
         </div>
       </div>
