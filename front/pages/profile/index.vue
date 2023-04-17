d917a78cba32a7477b21667f95e2b72fc884a8c7
rmingon <ronanmingon69550@gmail.com>
2023-04-17 16:23:14 +0200

    add account interface

--- front/pages/profile/index.vue
+++ front/pages/profile/index.vue
@@ -1,3 +1,46 @@
 <template>
-  Account
+  <div class="bg-white flex w-full bg-white h-screen drop-shadow-xl rounded-lg">
+    <div class="w-1/5 border-r">
+      <div class="flex-col text-center">
+        <div class="justify-center flex mt-12">
+          <img src="https://api.dicebear.com/6.x/adventurer-neutral/svg?seed=Mittens" alt="avatar" class="w-24 rounded-full ring ring-2 ring-offset-4"/>
+        </div>
+        <div class="mt-3">
+            <span class="font-bold text-xl">Username</span>
+        </div>
+        <div class="mb-10">
+            <span class="text-gray-400">mymail@mail.fr</span>
+        </div>
+        <div class="h-10 flex items-center hover:bg-blue-100 transform duration-100 hover:border-r-2 hover:border-blue-600 cursor-pointer px-2 nuxt-link-exact-active text-sm">
+            <div class="mr-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 32 32"><path fill="currentColor" d="M16 4a5 5 0 1 1-5 5a5 5 0 0 1 5-5m0-2a7 7 0 1 0 7 7a7 7 0 0 0-7-7zm10 28h-2v-5a5 5 0 0 0-5-5h-6a5 5 0 0 0-5 5v5H6v-5a7 7 0 0 1 7-7h6a7 7 0 0 1 7 7z"/></svg>
+            </div>
+            <p class="font-light text-sm">Personnal information</p>
+        </div>
+        <div class="h-10 flex items-center hover:bg-blue-100 transform duration-100 hover:border-r-2 hover:border-blue-600 cursor-pointer px-2 nuxt-link-exact-active text-sm">
+            <div class="mr-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 32 32"><path fill="currentColor" d="M16 4a5 5 0 1 1-5 5a5 5 0 0 1 5-5m0-2a7 7 0 1 0 7 7a7 7 0 0 0-7-7zm10 28h-2v-5a5 5 0 0 0-5-5h-6a5 5 0 0 0-5 5v5H6v-5a7 7 0 0 1 7-7h6a7 7 0 0 1 7 7z"/></svg>
+            </div>
+            <p class="font-light text-sm">Notification</p>
+        </div>
+          <div class="h-10 flex items-center hover:bg-blue-100 transform duration-100 hover:border-r-2 hover:border-blue-600 cursor-pointer px-2 nuxt-link-exact-active text-sm">
+              <div class="mr-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 32 32"><path fill="currentColor" d="M16 4a5 5 0 1 1-5 5a5 5 0 0 1 5-5m0-2a7 7 0 1 0 7 7a7 7 0 0 0-7-7zm10 28h-2v-5a5 5 0 0 0-5-5h-6a5 5 0 0 0-5 5v5H6v-5a7 7 0 0 1 7-7h6a7 7 0 0 1 7 7z"/></svg>
+              </div>
+              <p class="font-light text-sm">Security settings</p>
+          </div>
+          <div class="h-10 flex items-center hover:bg-blue-100 transform duration-100 hover:border-r-2 hover:border-blue-600 cursor-pointer px-2 nuxt-link-exact-active text-sm">
+              <div class="mr-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 32 32"><path fill="currentColor" d="M16 4a5 5 0 1 1-5 5a5 5 0 0 1 5-5m0-2a7 7 0 1 0 7 7a7 7 0 0 0-7-7zm10 28h-2v-5a5 5 0 0 0-5-5h-6a5 5 0 0 0-5 5v5H6v-5a7 7 0 0 1 7-7h6a7 7 0 0 1 7 7z"/></svg>
+              </div>
+              <p class="font-light text-sm">Password change</p>
+          </div>
+      </div>
+    </div>
+    <div class="w-full">
+
+    </div>
+  </div>
 </template>
+<script setup lang="ts">
+</script>
