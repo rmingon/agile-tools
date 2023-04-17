0c5b9a708daf04113ea00fda0ea4e77e78abb264
rmingon <ronanmingon69550@gmail.com>
2023-04-17 16:22:51 +0200

    add button component

---
+++ front/components/commun/Button.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-center p-1">
+      {{ label }}
+  </div>
+</template>
+
+<script setup lang="ts">
+  interface Props {
+    label: string
+  }
+
+  const props = withDefaults(defineProps<Props>(), {
+    label: ""
+  })
+
+</script>
