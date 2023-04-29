695ded51edea3944e2029ae21341ceb6bc507d8e
rmingon <ronanmingon69550@gmail.com>
2023-04-29 21:19:13 +0200

    change sidebar style

--- front/components/Sidebar.vue
+++ front/components/Sidebar.vue
@@ -10,9 +10,9 @@
     </NuxtLink>
     <div class="flex-col justify-between">
       <div class="flex-col h-full">
-        <NuxtLink :to="tab.to" v-for="tab in tabs" class="h-10 flex items-center hover:bg-blue-100 transform duration-100 hover:border-r-2 hover:border-blue-600 cursor-pointer px-2 nuxt-link-exact-active text-sm">
+        <NuxtLink :to="tab.to" v-for="tab in tabs" class="my-2 mx-3 rounded-md h-10 flex items-center hover:bg-gray-100 transform duration-100 cursor-pointer px-2 nuxt-link-exact-active text-gray-500 active:bg-gradient-to-r active:from-blue-800 active:to-blue-400 active:text-white">
           <div class="mr-2" v-html="tab.icon"></div>
-          <p class="font-light text-sm">{{ tab.label }}</p>
+          <p class="font-bold">{{ tab.label }}</p>
         </NuxtLink>
       </div>
     </div>
@@ -29,27 +29,27 @@
   const tabs : Tabs[] = [
     {
       label: "Projects",
-      icon: '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 32 32"><path fill="currentColor" d="M30 20v-8h-8v3h-5V7a2 2 0 0 0-2-2h-5V2H2v8h8V7h5v18a2 2 0 0 0 2 2h5v3h8v-8h-8v3h-5v-8h5v3ZM8 8H4V4h4Zm16 16h4v4h-4Zm0-10h4v4h-4Z"/></svg>',
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="16" viewBox="0 0 32 32"><path fill="currentColor" d="M30 20v-8h-8v3h-5V7a2 2 0 0 0-2-2h-5V2H2v8h8V7h5v18a2 2 0 0 0 2 2h5v3h8v-8h-8v3h-5v-8h5v3ZM8 8H4V4h4Zm16 16h4v4h-4Zm0-10h4v4h-4Z"/></svg>',
       to: 'project'
     },
     {
       label: "Sprint planning",
-      icon: '<svg width="22" height="22" viewBox="0 0 32 32"><path d="M28 4H4a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h24a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm0 22H4V6h24z" fill="currentColor"/><path d="M21 20h-8a3.51 3.51 0 0 0-.88-1.86l3.8-6.64l-1.74-1l-3.78 6.62a3.35 3.35 0 0 0-.9-.12a3.5 3.5 0 1 0 3.15 5H21zM9.5 22a1.5 1.5 0 1 1 1.5-1.5A1.5 1.5 0 0 1 9.5 22z" fill="currentColor"/><path d="M19 8h7v2h-7z" fill="currentColor"/><path d="M19 12h4v2h-4z" fill="currentColor"/></svg>',
+      icon: '<svg width="22" height="16" viewBox="0 0 32 32"><path d="M28 4H4a2 2 0 0 0-2 2v20a2 2 0 0 0 2 2h24a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm0 22H4V6h24z" fill="currentColor"/><path d="M21 20h-8a3.51 3.51 0 0 0-.88-1.86l3.8-6.64l-1.74-1l-3.78 6.62a3.35 3.35 0 0 0-.9-.12a3.5 3.5 0 1 0 3.15 5H21zM9.5 22a1.5 1.5 0 1 1 1.5-1.5A1.5 1.5 0 0 1 9.5 22z" fill="currentColor"/><path d="M19 8h7v2h-7z" fill="currentColor"/><path d="M19 12h4v2h-4z" fill="currentColor"/></svg>',
       to: 'planning'
     },
     {
       label: "Daily stand-up",
-      icon: '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="1.5"><circle cx="12" cy="13" r="9" opacity=".5"/><path stroke-linecap="round" d="M15 13H9"/><path stroke-linecap="round" stroke-linejoin="round" d="m3.5 4.5l4-2.5m13 2.5l-4-2.5"/></g></svg>',
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="16" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-width="1.5"><circle cx="12" cy="13" r="9" opacity=".5"/><path stroke-linecap="round" d="M15 13H9"/><path stroke-linecap="round" stroke-linejoin="round" d="m3.5 4.5l4-2.5m13 2.5l-4-2.5"/></g></svg>',
       to: 'daily'
     },
     {
       label: "Sprint retrospective",
-      icon: '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 32 32"><path fill="currentColor" d="m24 2l-6 6l1.4 1.4L23 5.8V19c0 5-4 9-9 9c-1.6 0-3.1-.4-4.3-1.1c.4.1.8.1 1.3.1c4.4 0 8-3.6 8-8s-3.6-8-8-8s-8 3.6-8 8c0 6.1 4.9 11 11 11s11-4.9 11-11V5.8l3.6 3.6L30 8l-6-6zM5 19.6V19c0-3.3 2.7-6 6-6s6 2.7 6 6s-2.7 6-6 6c-3.1 0-5.7-2.4-6-5.4z"/></svg>',
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="16" viewBox="0 0 32 32"><path fill="currentColor" d="m24 2l-6 6l1.4 1.4L23 5.8V19c0 5-4 9-9 9c-1.6 0-3.1-.4-4.3-1.1c.4.1.8.1 1.3.1c4.4 0 8-3.6 8-8s-3.6-8-8-8s-8 3.6-8 8c0 6.1 4.9 11 11 11s11-4.9 11-11V5.8l3.6 3.6L30 8l-6-6zM5 19.6V19c0-3.3 2.7-6 6-6s6 2.7 6 6s-2.7 6-6 6c-3.1 0-5.7-2.4-6-5.4z"/></svg>',
       to: 'retro'
     },
     {
       label: "Sprint review",
-      icon: '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 32 32"><path fill="currentColor" d="M23.9 10c-.6-.6-1.5-1-2.4-1s-1.8.4-2.4 1l-1.4-1.4c1-1 2.4-1.6 3.8-1.6s2.9.6 3.8 1.6L23.9 10z"/><circle cx="24" cy="5" r="1" fill="currentColor"/><circle cx="19" cy="5" r="1" fill="currentColor"/><path fill="currentColor" d="M15 0h13c1.1 0 2 .9 2 2v10c0 1.1-.9 2-2 2h-3.5l-1.7 3l-1.8-1l2.3-4H28V2H15v10h5v2h-5c-1.1 0-2-.9-2-2V2c0-1.1.9-2 2-2zm5 19v6c0 .6-.4 1-1 1h-5v2h5c1.7 0 3-1.3 3-3v-6h-2zm-4.6 1.4c-.8 1-2.1 1.6-3.4 1.6s-2.6-.6-3.4-1.6L7 21.6C8.2 23 10 24 12 24s3.8-.9 5-2.4l-1.6-1.2z"/><circle cx="15.5" cy="17.5" r="1.5" fill="currentColor"/><circle cx="8.5" cy="17.5" r="1.5" fill="currentColor"/><path fill="currentColor" d="M10.1 26H5c-.6 0-1-.4-1-1V14c0-.6.4-1 1-1h5v-2H5c-1.7 0-3 1.3-3 3v11c0 1.7 1.3 3 3 3h4l2.3 4l1.7-1l-2.9-5z"/></svg>',
+      icon: '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="16" viewBox="0 0 32 32"><path fill="currentColor" d="M23.9 10c-.6-.6-1.5-1-2.4-1s-1.8.4-2.4 1l-1.4-1.4c1-1 2.4-1.6 3.8-1.6s2.9.6 3.8 1.6L23.9 10z"/><circle cx="24" cy="5" r="1" fill="currentColor"/><circle cx="19" cy="5" r="1" fill="currentColor"/><path fill="currentColor" d="M15 0h13c1.1 0 2 .9 2 2v10c0 1.1-.9 2-2 2h-3.5l-1.7 3l-1.8-1l2.3-4H28V2H15v10h5v2h-5c-1.1 0-2-.9-2-2V2c0-1.1.9-2 2-2zm5 19v6c0 .6-.4 1-1 1h-5v2h5c1.7 0 3-1.3 3-3v-6h-2zm-4.6 1.4c-.8 1-2.1 1.6-3.4 1.6s-2.6-.6-3.4-1.6L7 21.6C8.2 23 10 24 12 24s3.8-.9 5-2.4l-1.6-1.2z"/><circle cx="15.5" cy="17.5" r="1.5" fill="currentColor"/><circle cx="8.5" cy="17.5" r="1.5" fill="currentColor"/><path fill="currentColor" d="M10.1 26H5c-.6 0-1-.4-1-1V14c0-.6.4-1 1-1h5v-2H5c-1.7 0-3 1.3-3 3v11c0 1.7 1.3 3 3 3h4l2.3 4l1.7-1l-2.9-5z"/></svg>',
       to: 'review'
     }
   ]
